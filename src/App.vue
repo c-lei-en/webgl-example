660f8cae1d14436ebbfa663a09ee973ff841a8c1
c-lei-en <chenglei713@gmail.com>
2021-03-29 09:51:51 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,14 @@
 
 <style lang="less">
 html, body, #app {
+  height: 100%;
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
 </style>
