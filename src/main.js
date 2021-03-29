41c9cd9e5234d517fba8f9990ba48c294ae9f2b3
c-lei-en <chenglei713@gmail.com>
2021-03-29 14:37:30 +0800

    引入webgl相关函数库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/components/lib/cuon-matrix.js'
+import '@/components/lib/cuon-utils.js'
+import '@/components/lib/webgl-debug.js'
+import '@/components/lib/webgl-utils.js'
 
 Vue.config.productionTip = false
 
